44f81a7454d7123e87da9bf945e0fcb81dbc9f94
Nate <nate@rec13.com>
2018-03-22 08:39:41 -0400

    Initial Commit

---
+++ cnTodo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cnTodo
+//
+//  Created by Merinet cineus on 3/22/18.
+//  Copyright © 2018 rec13. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
